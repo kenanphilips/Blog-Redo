a0c892e49ef558fa1a074132821eb7d0dfb6a826
Kenan Philips <philipskenan@gmail.com>
2016-10-22 01:04:50 -0700

    Add Ajax for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,36 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    # @post = Post.find(params[:post_id])
+    # @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    @comment = Comment.new(params.require(:comment).permit(:name, :body))
+    @post = Post.find params[:post_id]
+    @comment.post = @post
 
-    redirect_to post_path(@post)
+    respond_to do |format|
+
+      if @comment.save
+        format.html { redirect_to post_path(@post) }
+        # format.js { render js: "alert('Does this browser support JS?');" }
+        format.js { render :create_success } # will look for file in view called comments/create_success.js.erb
+      else
+        format.html { render "/posts/show" }
+        format.js { render :create_failure }
+        # will look for file in views called comments/create_failure.js.erb
+      end
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+    # @comment = @post.comments.find(params[:id])
+    @comment = Comment.find params[:id]
     @comment.destroy
 
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post) }
+      # format.js { render :destroy_success }
+      format.js { render } # render /app/views/comments/destroy.js.erb
+    end
   end
 end
